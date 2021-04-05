882b8b54a916706658b5e030f7a1551966e92d17
Rizielle Corpin <riziellec@careercruising.com>
2021-04-05 11:44:56 +0200

    Init

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kommen</h1>
+    </div>
+  );
+}
+
+export default App;
